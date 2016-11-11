799d21db5f0421a84f2afa4e265e6424191156f9
ishaaann <ishanhanda1@gmail.com>
2016-11-11 23:23:11 +0530

    Mouse Clicks & Screen Resize

--- client.py
+++ client.py
@@ -1,38 +1,135 @@
 import socket
 import os
 from screenfeed import ScreenFeed
-
-host = '127.0.0.1'
-port = 9090
-buffer_length = 1000
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-sc = ScreenFeed('Teamviewer')
-
-while True:
-	frame = sc.get_frame()
-	bytes_sent = 0
-	len_sent = 0
-	length = len(frame)
-	length_str = str(length).zfill(10)
-
-	while len_sent < 10:
-		sent = s.send(length_str[len_sent:])
-		if sent == 0:
-			raise RuntimeError("Socket Connection Broken")
-		len_sent += sent
-
-	while bytes_sent < length:
-		sent = s.send(frame[bytes_sent:])
-		if sent == 0:
-			raise RuntimeError("Socket Connection Broken")
-		bytes_sent += sent
+import time
+import pyautogui
 
 
+host = ''
+port_1 = 9090
+port_2 = 9091
+port_3 = 9092
+broadcast_port = 10100
+host_default = ''
+buffer_length = 1000000000
 
-
-
-
-
+#Broadcasting to server
+port = 10100
+msg = socket.gethostbyname(socket.gethostname())
+dest = ('<broadcast>',port)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.sendto(msg, dest)
 s.close()
+
+#Receving Broadcast from client
+TCP_IP = ''
+TCP_IP_2 = ''
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((TCP_IP_2,port))
+message, address = s.recvfrom(10104)            
+ad = ''.join(str(address));
+ad1 = ad.split()
+ad2= ad1[0]
+ad3=ad2[2:15]
+ad3 = ad3.replace("'", "")
+ad3 = ad3.replace(",", "")
+TCP_IP_2 = str(ad3)
+TCP_IP = message
+print TCP_IP_2
+print TCP_IP
+host = TCP_IP
+s.close()
+time.sleep(1)
+
+
+
+
+
+# Socket for receiving mouse events
+soc_event = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc_event.connect((host, port_1))
+
+#Socket for sending frames
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port_2))
+
+#Socket for sending frame sizes
+soc_frame_size = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc_frame_size.connect((host, port_3))
+
+
+#Sending screen size so that the user can resize the screen
+screen_width, screen_height = pyautogui.size()
+wsent = 0
+hsent = 0
+heightstr = str(screen_height).zfill(8)
+widthstr = str(screen_width).zfill(8)
+
+while hsent < 8:
+	sent = soc_frame_size.send(heightstr[hsent:])
+	if sent == 0:
+		raise RuntimeError("Socket connection broken")
+	hsent += sent
+while wsent < 8:
+	sent = soc_frame_size.send(widthstr[wsent:])
+	if sent == 0:
+		raise RuntimeError("Socket connection broken")
+	wsent += sent
+
+soc_frame_size.close()
+
+
+#Sending frames in parent and receiving mouse clicks in child
+sc = ScreenFeed('Teamviewer')
+pid = os.fork()
+if(pid!=0):
+	
+	while True:
+		frame = sc.get_frame()
+		bytes_sent = 0
+		len_sent = 0
+		length = len(frame)
+		length_str = str(length).zfill(10)
+
+		while len_sent < 10:
+			sent = s.send(length_str[len_sent:])
+			if sent == 0:
+				raise RuntimeError("Socket Connection Broken")
+			len_sent += sent
+
+		while bytes_sent < length:
+			sent = s.send(frame[bytes_sent:])
+			if sent == 0:
+				raise RuntimeError("Socket Connection Broken")
+			bytes_sent += sent
+
+else:
+
+	
+	while True:
+		try:
+			type = soc_event.recv(2)
+			type = int(type)
+			if type == 1:
+				x = soc_event.recv(4)
+				x = int(x)
+				y = soc_event.recv(4)
+				y = int(y)
+				pyautogui.moveTo(x,y)
+				pyautogui.click()
+			if type == 2:
+				x = soc_event.recv(4)
+				x = int(x)
+				y = soc_event.recv(4)
+				y = int(y)
+				pyautogui.moveTo(x,y)
+				pyautogui.click(button='right')
+		except Exception as error:
+			print ("Error is:", a)  
+		
+s.close()
+soc_frame_size.close()
+soc_event.close()
--- server.py
+++ server.py
@@ -1,41 +1,184 @@
 import socket
+import time
+import os,sys
+import PIL
+import Tkinter as tk
+import io
+import cv2
+import numpy as np
+from PIL import Image, ImageTk
 from screenfeed import ScreenFeed
+import traceback
 
-host = '127.0.0.1'
-port = 9090
 
+
+host = ''
+port_1 = 9090
+port_2 = 9091
+port_3 = 9092
+broadcast_port = 10100
+host_default = ''
+buffer_length = 1000000000
+
+#Receving broadcast from client
+TCP_IP = ''
+port = 10100
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((TCP_IP,port))
+message, address = s.recvfrom(10104)            
+ad = ''.join(str(address));
+ad1 = ad.split()
+ad2= ad1[0]
+ad3=ad2[2:15]
+s.close()
+ad3 = ad3.replace("'", "")
+ad3 = ad3.replace(",", "")
+TCP_IP = str(ad3)
+
+#Sending broadcast to server
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 0))
+msg = s.getsockname()[0]
+TCP_IP = msg
+print TCP_IP
+dest = ('<broadcast>',10100)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.sendto(TCP_IP, dest)
+host = TCP_IP
+s.close()
+
+
+#Socket for sending mouse events
+soc_event = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc_event.bind((host,port_1))
+soc_event.listen(5)
+
+
+#Socket for receiving frames
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host,port))
+s.bind((host,port_2))
 s.listen(5)
 
+
+#Socket for receiving frame size
+soc_recv_size = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc_recv_size.bind((host,port_3))
+soc_recv_size.listen(5)
+
+
+conn1, addr1 = soc_recv_size.accept()
+print 'Connection address for receiving frame sizes:', addr1
+
+#Receiving Frame Sizes
+hrec = 0
+wrec = 0
+harray = []
+warray = []
+while hrec < 8:
+	chunk = conn1.recv(8 - hrec)
+	if chunk == '':
+		raise RuntimeError("Socket connection broken")
+	harray.append(chunk)
+	hrec +=  len(chunk)
+heightstr = ''.join(harray)
+screen_height = int(heightstr)
+while wrec < 8:
+	chunk = conn1.recv(8 - wrec)
+	if chunk == '':
+		raise RuntimeError("Socket connection broken")
+	warray.append(chunk)
+	wrec +=  len(chunk)
+widthstr = ''.join(warray)
+screen_width = int(widthstr)
+conn1.close()
+soc_recv_size.close()
+
+root = tk.Tk()
+root.geometry(str(screen_width) + "x" + str(screen_height))
+
+
+
+conn2, addr2 = soc_event.accept()
+print 'Connection address for sending frame sizes:', addr2
 conn, addr = s.accept()
-print 'Connection address:', addr
+print 'Connection address for receiving frames:', addr
 sc = ScreenFeed('Teamviewer')
 
-while 1:
-	tot_recv = 0
-	len_recv = 0
-	msg = []
-	len_msg = []
-	while len_recv < 10:
-		chunk = conn.recv(10 - tot_recv)
-		if chunk == '':
-			raise RuntimeError("Socket connection broken")
-		len_msg.append(chunk)
-		len_recv += len(chunk)
-	len_str = ''.join(len_msg)
-	length = int(len_str)
+#Sending Events for left and right mouse clicks
+def left(event):
+	type = 1
+	conn2.send(str(type).zfill(2))
+	conn2.send(str(event.x).zfill(4))
+	conn2.send(str(event.y).zfill(4))
 
-	while tot_recv < length:
-		chunk = conn.recv(length - tot_recv)
-		if chunk == '':
-			raise RuntimeError("Socket connection broken")
-		msg.append(chunk)
-		tot_recv += len(chunk)
-	msg = ''.join(msg)
-	sc.set_frame(msg)
+def right(event):
+	type = 2
+	conn2.send(str(type).zfill(2))
+	conn2.send(str(event.x).zfill(4))
+	conn2.send(str(event.y).zfill(4))
 
-conn.close()
+def quit(event):
+	print ("Double Click, so let's stop", repr(event))
+
+#Receving frames and displaying it
+counter = 0
+pid = os.fork()
+if(pid != 0):
+	while 1:
+		tot_recv = 0
+		len_recv = 0
+		msg = []
+		len_msg = []
+		while len_recv < 10:
+			chunk = conn.recv(10 - tot_recv)
+			if chunk == '':
+				raise RuntimeError("Socket connection broken")
+			len_msg.append(chunk)
+			len_recv += len(chunk)
+		len_str = ''.join(len_msg)
+		length = int(len_str)
+
+		while tot_recv < length:
+			chunk = conn.recv(length - tot_recv)
+			if chunk == '':
+				raise RuntimeError("Socket connection broken")
+			msg.append(chunk)
+			tot_recv += len(chunk)
+		msg = ''.join(msg)
+		pil_bytes = io.BytesIO(msg)
+		pil_image = Image.open(pil_bytes)
+		img_np = np.array(pil_image)
+		frame = cv2.cvtColor(np.array(img_np), cv2.COLOR_GRAY2RGB)
+		pil_image = Image.fromarray(frame)
+		pil_image = pil_image.resize((screen_width,screen_height), PIL.Image.ANTIALIAS)
+		photo = ImageTk.PhotoImage(image = pil_image)	
+		if (counter == 0) :	
+				label = tk.Label(root, image= photo)
+				label.bind('<Button-1>', left)
+	 			label.bind('<Button-3>', right)
+				label.grid()
+				label.update()
+		else:
+				label.configure(image = photo)
+				label.image = photo
+				label.update()
+		counter = counter + 1
+		conn.close
+else:
+
+	running = True
+	while running:
+		time.sleep(0.1)
+
+    
+root.mainloop()
+
+s.close()
+soc_event.close()
+
 
 
 
