4928d51a653e7a0235e01216469530e26d4fc2cf
ishaaann <ishanhanda1@gmail.com>
2016-11-13 18:45:09 +0530

    Getting Things To Work Again

--- client.py
+++ client.py
@@ -1,36 +1,28 @@
 import socket
+import cv2
+import numpy as np
 import os
-from screenfeed import ScreenFeed
 import time
-import pyautogui
+import pyautogui 
+from PIL import Image
+import pyscreenshot as ImageGrab
 import io
 
-
-host = ''
-port_1 = 9090
-port_2 = 9091
-port_3 = 9092
-broadcast_port = 10100
-host_default = ''
-buffer_length = 1000000000
-
-#Broadcasting to server
 port = 10100
 msg = socket.gethostbyname(socket.gethostname())
 dest = ('<broadcast>',port)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.sendto(msg, dest)
+print msg
 s.close()
-
-#Receving Broadcast from client
 TCP_IP = ''
 TCP_IP_2 = ''
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind((TCP_IP_2,port))
-message, address = s.recvfrom(10104)            
+message, address = s.recvfrom(1024)            
 ad = ''.join(str(address));
 ad1 = ad.split()
 ad2= ad1[0]
@@ -39,28 +31,32 @@ ad3 = ad3.replace("'", "")
 ad3 = ad3.replace(",", "")
 TCP_IP_2 = str(ad3)
 TCP_IP = message
-host = TCP_IP
+#print TCP_IP_2
+print TCP_IP
 s.close()
 time.sleep(1)
 
+TCP_IP =TCP_IP.strip()
+
+
+#TCP_IP = '127.0.0.1'
+TCP_PORT = 5006
+MESSAGE = "Hello, World!"
+
+TCP_PORT_2 = 9656
+TCP_PORT_3 = 6969
+
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2.connect((TCP_IP, TCP_PORT_3))
 
 
 
-
-# Socket for receiving mouse events
-soc_event = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc_event.connect((host, port_1))
-
-#Socket for sending frames
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port_2))
+s.connect((TCP_IP, TCP_PORT))
 
-#Socket for sending frame sizes
-soc_frame_size = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc_frame_size.connect((host, port_3))
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s1.connect((TCP_IP, TCP_PORT_2))
 
-
-#Sending screen size so that the user can resize the screen
 screen_width, screen_height = pyautogui.size()
 wsent = 0
 hsent = 0
@@ -68,64 +64,71 @@ heightstr = str(screen_height).zfill(8)
 widthstr = str(screen_width).zfill(8)
 
 while hsent < 8:
-	sent = soc_frame_size.send(heightstr[hsent:])
+	sent = s2.send(heightstr[hsent:])
 	if sent == 0:
 		raise RuntimeError("Socket connection broken")
 	hsent += sent
 while wsent < 8:
-	sent = soc_frame_size.send(widthstr[wsent:])
+	sent = s2.send(widthstr[wsent:])
 	if sent == 0:
 		raise RuntimeError("Socket connection broken")
 	wsent += sent
 
-soc_frame_size.close()
 
+s2.close()
 
-#Sending frames in parent and receiving mouse clicks in child
-sc = ScreenFeed('Teamviewer')
 pid = os.fork()
-if(pid!=0):
-	
-	while True:
-		frame = sc.get_frame()
-		bytes_sent = 0
-		len_sent = 0
-		length = len(frame)
-		length_str = str(length).zfill(10)
+if pid!=0:
+   
+    while 1:
+	try:
+        	img = ImageGrab.grab()
+    		img_np = np.array(img)
+        	frame = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        	pil_im = Image.fromarray(frame)
+        	b = io.BytesIO()
+        	pil_im.save(b, 'jpeg')
+        	im_bytes = b.getvalue()
+        
+        	totalsent = 0
+        	metasent = 0
+		length =len(im_bytes)
+		lengthstr=str(length).zfill(8)
 
-		while len_sent < 10:
-			sent = s.send(length_str[len_sent:])
-			if sent == 0:
-				raise RuntimeError("Socket Connection Broken")
-			len_sent += sent
+		while metasent < 8 :
+		    sent = s.send(lengthstr[metasent:])
+		    if sent == 0:
+		        raise RuntimeError("Socket connection broken")
+		    metasent += sent
 
-		while bytes_sent < length:
-			sent = s.send(frame[bytes_sent:])
-			if sent == 0:
-				raise RuntimeError("Socket Connection Broken")
-			bytes_sent += sent
 
+		while totalsent < length :
+		    sent = s.send(im_bytes[totalsent:])
+		    if sent == 0:
+		        raise RuntimeError("Socket connection broken")
+		    totalsent += sent
+	except Exception as e:
+		print ("Error is: ", e)
 else:
-
-	
+	child = pid
 	while True:
-		try:
-			type = soc_event.recv(2)
+		try:	    
+			type = s1.recv(2)
 			type = int(type)
 			if type == 1:
-				x = soc_event.recv(4)
+				x = s1.recv(4)
 				x = int(x)
-				y = soc_event.recv(4)
+				y = s1.recv(4)
 				y = int(y)
 				pyautogui.moveTo(x,y)
 				pyautogui.click()
 			if type == 2:
-				x = soc_event.recv(4)
+				x = s1.recv(4)
 				x = int(x)
-				y = soc_event.recv(4)
+				y = s1.recv(4)
 				y = int(y)
 				pyautogui.moveTo(x,y)
-				pyautogui.click(button='right')
+				pyautogui.click(button='right')  
 			if type == 0:
 				x = s1.recv(20)
 				if x == 'BackSpace':
@@ -160,12 +163,12 @@ else:
 					pyautogui.press('right')
 				elif x == 'Left':
 					pyautogui.press('left')
+				elif x == 'period':
+					pyautogui.press('.')
+				elif x == 'apostrophe':
+					pyautogui.press('"')
 				elif x == 'Term':
 					pyautogui.hotkey('ctrl', 'alt', 't')
-				elif x == "period":
-					pyautogui.press('.')
-				elif x == "apostrophe":
-					pyautogui.press('"')
 				else:
 					pyautogui.press(x)
 			if type == 3:
@@ -193,10 +196,10 @@ else:
 				s.close()
 				s1.close()
 				os.system("kill "+ str(child))
-				sys.exit()			
-		except Exception as error:
-			print ("Error is:", a)  
-		
+				sys.exit()				
+	
+		except Exception as a:
+			time.sleep(2)
+			print ("Error is: ", a)        
 s.close()
-soc_frame_size.close()
-soc_event.close()
+s1.close()
--- server.py
+++ server.py
@@ -1,32 +1,20 @@
 import socket
+import pyxhook
 import time
 import os,sys
 import PIL
 import Tkinter as tk
 import io
-import pyxhook
 import cv2
+from Tkinter import *
 import numpy as np
 from PIL import Image, ImageTk
-import PIL.Image
-from screenfeed import ScreenFeed
-from Tkinter import *
 import traceback
 from tkFileDialog import askopenfilename
-
 
 child = 0
 new_width = 0
 new_height = 0
-host = ''
-port_1 = 9090
-port_2 = 9091
-port_3 = 9092
-broadcast_port = 10100
-host_default = ''
-buffer_length = 1000000000
-
-#Receving broadcast from client
 TCP_IP = ''
 port = 10100
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -38,66 +26,55 @@ ad = ''.join(str(address));
 ad1 = ad.split()
 ad2= ad1[0]
 ad3=ad2[2:15]
+#print ad3, message
 s.close()
 ad3 = ad3.replace("'", "")
 ad3 = ad3.replace(",", "")
 TCP_IP = str(ad3)
 
-#Sending broadcast to server
+#time.sleep(1)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('8.8.8.8', 0))
 msg = s.getsockname()[0]
 TCP_IP = msg
+print TCP_IP
 dest = ('<broadcast>',10100)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.sendto(TCP_IP, dest)
-host = TCP_IP
 s.close()
 
 
-#Socket for sending mouse events
-soc_event = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc_event.bind((host,port_1))
-soc_event.listen(5)
+#TCP_IP = '127.0.0.1'
 
+TCP_PORT = 5006
+BUFFER_SIZE = 10000000
 
-#Socket for receiving frames
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host,port_2))
-s.listen(5)
-
-
-#Socket for receiving frame size
-soc_recv_size = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc_recv_size.bind((host,port_3))
-soc_recv_size.listen(5)
-
-#Sending Events for left, right mouse clicks, keyboards, file transfer and hotkeys
-def left(event):
-	type = 1
-	conn2.send(str(type).zfill(2))
-	conn2.send(str(event.x).zfill(4))
-	conn2.send(str(event.y).zfill(4))
-
-def right(event):
-	type = 2
-	conn2.send(str(type).zfill(2))
-	conn2.send(str(event.x).zfill(4))
-	conn2.send(str(event.y).zfill(4))
-
+TCP_PORT_2 = 9656
+TCP_PORT_3 = 6969
 def kbevent( event ):
     	type = 0
 	conn1.send(str(type).zfill(2))  	
 	conn1.send(event.Key)
 
-def quit(event):
-	print ("Double Click, so let's stop", repr(event))
 
-def sendHotKey():
-	type = 0
-	conn1.send(str(type).zfill(2))  	
-	conn1.send("Term")	
+def left(event):
+	type = 1
+	x = ((event.x))
+	y = ((event.y))	
+	conn1.send(str(type).zfill(2))
+	conn1.send(str(event.x).zfill(4))
+	conn1.send(str(event.y).zfill(4))
+	
+
+
+def right(event):
+	type = 2
+	x = ((event.x))
+	y = ((event.y))
+	conn1.send(str(type).zfill(2))
+	conn1.send(str(x).zfill(4))
+	conn1.send(str(y).zfill(4))
 
 def _resize_image(event):
         origin = (0,0)
@@ -109,6 +86,10 @@ def _resize_image(event):
 		new_width = event.width
 		print new_width, new_height	
 
+def sendHotKey():
+	type = 0
+	conn1.send(str(type).zfill(2))  	
+	conn1.send("Term")	
 
 def sendFile():
 	try:
@@ -149,18 +130,27 @@ def closeButton():
 	sys.exit()
 
 
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2.bind((TCP_IP, TCP_PORT_3))
+s2.listen(5)
 
 
-conn1, addr1 = soc_recv_size.accept()
-print 'Connection address for receiving frame sizes:', addr1
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(5)
 
-#Receiving Frame Sizes
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s1.bind((TCP_IP, TCP_PORT_2))
+s1.listen(5)
+
+conn2, addr2 = s2.accept()
+
 hrec = 0
 wrec = 0
 harray = []
 warray = []
 while hrec < 8:
-	chunk = conn1.recv(8 - hrec)
+	chunk = conn2.recv(8 - hrec)
 	if chunk == '':
 		raise RuntimeError("Socket connection broken")
 	harray.append(chunk)
@@ -168,99 +158,112 @@ while hrec < 8:
 heightstr = ''.join(harray)
 screen_height = int(heightstr)
 while wrec < 8:
-	chunk = conn1.recv(8 - wrec)
+	chunk = conn2.recv(8 - wrec)
 	if chunk == '':
 		raise RuntimeError("Socket connection broken")
 	warray.append(chunk)
 	wrec +=  len(chunk)
 widthstr = ''.join(warray)
 screen_width = int(widthstr)
-conn1.close()
-soc_recv_size.close()
+conn2.close()
 
-new_width = screen_width/2
-new_height = screen_height/2
+new_width = screen_width
+new_height = screen_height
+
+s2.close()
+conn, addr = s.accept()
+conn1, addr1 = s1.accept()
 
 root = tk.Tk()
 root.title("Harambe")
-root.geometry(str(screen_width) + "x" + str(screen_height))
-
+root.geometry(str(screen_width) + "x" + str(screen_height + 40))
+#root.bind("<Configure>",_resize_image)
 openTerm = tk.Button(root, text = "Open Terminal", padx = 10, pady = 10, justify = CENTER, command = sendHotKey)
 openTerm.place(y = (new_height), x = (new_width - 400))   	
 sendFile = tk.Button(root, text = "Send File", padx = 10, pady = 10, justify = CENTER, command = sendFile)
 sendFile.place(y = (new_height), x = (400))   
 closeB = tk.Button(root, text = "X", padx = 10, pady = 10, justify = CENTER, command = closeButton)
-closeB.place(y = (new_height), x = 50)
+closeB.place(y = (new_height), x = 50)   
 
 
-conn2, addr2 = soc_event.accept()
-print 'Connection address for sending frame sizes:', addr2
-conn, addr = s.accept()
-print 'Connection address for receiving frames:', addr
-sc = ScreenFeed('Teamviewer')
-
-
-#Receving frames and displaying it
 counter = 0
 pid = os.fork()
-if(pid != 0):
+if pid != 0:
+	print "Starting: "
 	while 1:
-		tot_recv = 0
-		len_recv = 0
-		msg = []
-		len_msg = []
-		while len_recv < 10:
-			chunk = conn.recv(10 - tot_recv)
-			if chunk == '':
+		try:
+			totrec=0
+			metarec=0
+			msgArray = []
+			metaArray = []
+			while metarec < 8:
+			    chunk = conn.recv(8 - metarec)
+			    if chunk == '':
 				raise RuntimeError("Socket connection broken")
-			len_msg.append(chunk)
-			len_recv += len(chunk)
-		len_str = ''.join(len_msg)
-		length = int(len_str)
+			    metaArray.append(chunk)
+			    metarec += len(chunk)
+			lengthstr= ''.join(metaArray)
+			length=int(lengthstr)
 
-		while tot_recv < length:
-			chunk = conn.recv(length - tot_recv)
-			if chunk == '':
+			while totrec<length :
+			    chunk = conn.recv(length - totrec)
+			    if chunk == '':
 				raise RuntimeError("Socket connection broken")
-			msg.append(chunk)
-			tot_recv += len(chunk)
-		msg = ''.join(msg)
-		pil_bytes = io.BytesIO(msg)
-		pil_image = PIL.Image.open(pil_bytes)
-		img_np = np.array(pil_image)
-		frame = cv2.cvtColor(np.array(img_np), cv2.COLOR_GRAY2RGB)
-		pil_image = PIL.Image.fromarray(frame)
-		pil_image = pil_image.resize((screen_width,screen_height), PIL.Image.ANTIALIAS)
-		photo = ImageTk.PhotoImage(image = pil_image)	
-		if (counter == 0) :	
+			    msgArray.append(chunk)
+			    totrec += len(chunk)
+			msgArray = ''.join(msgArray)
+			pil_bytes = io.BytesIO(msgArray)
+			pil_image = Image.open(pil_bytes)
+			img_np = np.array(pil_image)
+			frame = cv2.cvtColor(np.array(img_np), cv2.COLOR_BGR2RGB)
+			pil_image = Image.fromarray(frame)
+			pil_image = pil_image.resize((new_width,new_height), PIL.Image.ANTIALIAS)
+			photo = ImageTk.PhotoImage(image = pil_image)	
+			if (counter == 0) :	
 				label = tk.Label(root, image= photo)
 				label.bind('<Button-1>', left)
-	 			label.bind('<Button-3>', right)
+ 				#label.bind('<Button-2>', middle)
+ 				label.bind('<Button-3>', right)
+				#label.bind('<Button-4>', scroll_up)
+ 				#label.bind('<Button-5>', scroll_down)
 				label.grid()
 				label.update()
-		else:
+			else:
 				label.configure(image = photo)
 				label.image = photo
 				label.update()
-		counter = counter + 1
-		conn.close
+			counter = counter + 1
+		
+	    		
+		except Exception as a:
+			print ("Error is: ", a)
+	    	
+    	conn.close()
+	print "Close conn"
 else:
+	child = pid
+	try:	    
+	    
+	    hookman = pyxhook.HookManager()
+	    #Define our callback to fire when a key is pressed down
+	    hookman.KeyDown = kbevent
+	    #Hook the keyboard
+	    hookman.HookKeyboard()
+	    #Start our listener
+	    hookman.start()
+		
+	    #Create a loop to keep the application running
+	   
+	    running = True
 
-	hookman = pyxhook.HookManager()
-	hookman.KeyDown = kbevent
-	hookman.HookKeyboard()
-	hookman.start()
-	running = True
-	while running:
+	    while running:
 		time.sleep(0.1)
-	hookman.cancel()
-
-    
+		
+	    #Close the listener when we are done
+	    hookman.cancel()
+	except Exception as a:
+		print "Error is: ", a
 root.mainloop()
 
+s1.close()
 s.close()
-soc_event.close()
-
-
-
-
